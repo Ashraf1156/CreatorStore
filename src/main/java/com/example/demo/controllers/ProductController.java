97c18bb4ffc1f36e58b24dff0614467dfdd56256
Ashraf <mdashrafhussain5611@gmail.com>
2026-05-19 16:05:52 +0530

    chore: Created the Service Layer and Worked on the Business logic

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controllers;
 
 import com.example.demo.entities.Product;
+import com.example.demo.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 
 @RestController
@@ -13,29 +16,32 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProductController {
 
+    private final ProductService productService;
 
     @PostMapping
     public Product createProduct(@Valid Product product){
-        return null;
+        return productService.createProduct(product); //passing the request to the service layer to create the product
     }
 
     @PutMapping("/{id}")
     public Product updateProduct(@PathVariable Long id, @Valid @RequestBody Product product){
-        return null;
+        return productService.updateProduct(id,product);
     }
 
     @GetMapping
     public List<Product> getProducts(){
-        return null;
+        return productService.getProducts();
     }
 
     @GetMapping("/{id}")
-    public void getProductById(@PathVariable Long Id){
+    public Product getProductById(@PathVariable Long id){
+        return productService.getProductById(id);
 
     }
 
     @DeleteMapping("/{id}")
-    public void deleteProduct(@PathVariable Long Id){
+    public void deleteProduct(@PathVariable Long id){
+        productService.deleteProduct(id);
 
     }
 
@@ -46,3 +52,6 @@ public class ProductController {
 //When we follow the solid principles of MVC architecture, We will separate the Business-logic with the end-points
 
 //Whatever the Entities, Repositories, Controllers(end-points) should go-through the Service layer(where our Business logic recides)
+
+// CONTROLLER CLASS ONLY RESPONSIBILITY IS JUST TO MAP THE CORRECT ENDPOINTS AND HANDING OF ITS TASKS TO ITS SERVICE LAYER
+// SERVICE LAYER's RESPONSIBILITY IS DOING THE BUSINESS LOGIC AND INTERACTING WITH DATABASE WHEN NECESSARY
